78f2aa5c0f6e3fdcdff3fb7d4fdede3122ca6520
damian14443 <damian14443@gmail.com>
2020-10-30 11:29:39 +0100

    change scale of background

--- src/Characters/Character_template.gd
+++ src/Characters/Character_template.gd
@@ -1,5 +1,5 @@
 extends KinematicBody2D
 
-var speed = 20; # base speed for all characters
-var max_speed = 200; # base max_speed for all characters
+var speed = 30; # base speed for all characters
+var max_speed = 300; # base max_speed for all characters
 var resistance = 0.1; # 0.1s
