1c4f04444d9ccb6280f94d856f15070fc6af0368
damian14443 <damian14443@gmail.com>
2020-11-12 01:06:48 +0100

    collision and basic enemy

---
+++ src/Characters/Enemies/Slime.gd
@@ -0,0 +1,61 @@
+extends "res://Characters/Character_template.gd"
+
+onready var navigation = get_tree().get_root().find_node("Navigation2D", true, false)
+onready var player = get_parent().get_parent().get_node("Player")
+
+var path = []
+var motion
+var player_in_pov = false
+
+export var enemy_speed = 0.5
+
+func _physics_process(delta):
+	move()
+	
+
+func move():
+	if  path.size() > 0:
+		# print("test")
+		motion = (path[0] - position).normalized() * (max_speed * enemy_speed)
+		#print(position, path[0])
+		#look_at(path[0])
+		move_and_slide(motion)
+		
+		print("(", int(path[0].x), ",", int(path[0].y), "), (", int(position.x),",", int(position.y), ")", player_in_pov)
+		print(path, path.size())
+		
+		if path[0].x - position.x < 5 and path[0].y - position.y < 5:
+			#print("(", int(path[0].x), ",", int(position.x), "), (", int(path[0].y),",", int(position.y), ")", player_in_pov)
+			#print(path)
+			path.remove(0)
+	
+	if is_on_wall():
+		print("test")
+		make_path()
+	
+	if player_in_pov and path.size() == 0:
+		make_path()
+
+func make_path():
+	path = navigation.get_simple_path(position, player.position, false)
+	path.remove(0)
+
+func _on_POV_body_entered(body):
+	player_in_pov = true
+#	path = navigation.get_simple_path(position, player.position, false)
+#	path.remove(0)
+	make_path()
+	#print(player)
+	#print(path)
+
+
+func _on_POV_body_exited(body):
+	player_in_pov = false
+	if path.size() < 1:
+		path = [];
+
+
+func _on_Player_move():
+	if player_in_pov and path.size() < 2:
+		print("test")
+		make_path()
--- src/Player.gd
+++ src/Player.gd
@@ -2,6 +2,8 @@ extends "res://Characters/Character_template.gd"
 
 var motion = Vector2()
 
+signal move
+
 func _physics_process(delta):
 	update_movement()
 	move_and_slide(motion)
@@ -10,15 +12,19 @@ func update_movement():
 	look_at(get_global_mouse_position())
 	if Input.is_action_pressed("player_down") and not Input.is_action_pressed("player_up"):
 		motion.y = clamp(motion.y + speed, 0, max_speed)
+		emit_signal("move")
 	elif Input.is_action_pressed("player_up") and not Input.is_action_pressed("player_down"):
 		motion.y = clamp(motion.y - speed, -max_speed, 0)
+		emit_signal("move")
 	else:
 		# lerp - linear interpolate
 		motion.y = lerp(motion.y, 0, resistance)
 		
 	if Input.is_action_pressed("player_left") and not Input.is_action_pressed("player_right"):
 		motion.x = clamp(motion.x - speed, -max_speed, 0)
+		emit_signal("move")
 	elif Input.is_action_pressed("player_right") and not Input.is_action_pressed("player_left"):
 		motion.x = clamp(motion.x + speed, 0, max_speed)
+		emit_signal("move")
 	else:
 		motion.x = lerp(motion.x, 0, resistance)
