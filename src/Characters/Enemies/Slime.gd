9c7eb4df33dd7216a968a13b924d9f80c7057aeb
damian14443 <damian14443@gmail.com>
2020-11-19 23:44:39 +0100

    change vase and slime collision polygon

--- src/Characters/Enemies/Slime.gd
+++ src/Characters/Enemies/Slime.gd
@@ -6,6 +6,8 @@ onready var player = get_parent().get_parent().get_node("Player")
 var path = []
 var motion
 var player_in_pov = false
+var current_point
+#var blocked = false
 
 export var enemy_speed = 0.5
 
@@ -25,6 +27,7 @@ func move():
 		#print(path, path.size())
 		#print(motion, path[0])
 		print(self.name, path)
+#		print(self.name, blocked)
 		
 		if path[0].x - position.x < 2 and path[0].y - position.y < 2:
 			#print("(", int(path[0].x), ",", int(position.x), "), (", int(path[0].y),",", int(position.y), ")", player_in_pov)
@@ -35,6 +38,11 @@ func move():
 #	if is_on_wall():
 #		#print("test")
 #		make_path()
+#	if blocked:
+		#var current_point = path[0]
+#		path = []
+#		path.insert(0, Vector2(-position.x * 0.5, -position.y * 0.5))
+#		blocked = false
 	
 	if player_in_pov and path.size() < 2:
 		make_path()
@@ -65,11 +73,18 @@ func _on_Player_move():
 
 func _on_HitBox_body_entered(body):
 	print(body.name)
+	print(body.name, " ", navigation)
 	if body.name == "Player":
+#		blocked = false
 		Global.hurt_player()
 	elif body.name == "TileMapItems":
-		path.insert(0, Vector2(-position.x, -position.y))
-		make_path()
+#		blocked = true
+#		path.insert(0, current_point)
+#		path = []
+#		path.insert(0, Vector2(-position.x * 0.5, -position.y * 0.5))
+#		print(body.position)
+#		make_path()
 		pass
 	elif body is KinematicBody2D:
 		make_path()
+#		blocked = false
