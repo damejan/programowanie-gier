a726b6a5d7e30ce8a06caabca2b5fdeaaa5c504e
damian14443 <damian14443@gmail.com>
2020-11-20 01:46:01 +0100

    detach inheritance(CharcterTemplate) from player

--- src/Player.gd
+++ src/Player.gd
@@ -1,4 +1,8 @@
-extends "res://Characters/Character_template.gd"
+extends KinematicBody2D
+
+var speed = 30; # base speed for all characters
+var max_speed = 300; # base max_speed for all characters
+var resistance = 0.1; # 0.1s
 
 var motion = Vector2()
 
